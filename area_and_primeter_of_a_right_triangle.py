3c415a097ba3c2c2b069f06b2063c068094337ba
Joseph Hyatt <josephnhyatt@gmail.com>
2019-12-17 16:47:32 -0800

    fixed math mistake

--- area_and_primeter_of_a_right_triangle.py
+++ area_and_primeter_of_a_right_triangle.py
@@ -4,7 +4,7 @@ x = float(input("Insert length of x: "))
 y = float(input("Insert length of y: "))
 z = math.sqrt((pow(x, 2) + pow(y, 2)))
 
-area = (x + y) / 2
+area = (x * y) / 2
 perimeter = x + y + z
 
 print("Area of right angled triangle = %.2f" % area)
