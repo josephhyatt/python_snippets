16c38e8da5ef1ad8bb9d3a4dd15440358d3960b0
Joseph Hyatt <josephnhyatt@gmail.com>
2019-12-17 16:46:13 -0800

    calculate the area and perimeter of a right triangle

---
+++ area_and_primeter_of_a_right_triangle.py
@@ -0,0 +1,11 @@
+import math
+
+x = float(input("Insert length of x: "))
+y = float(input("Insert length of y: "))
+z = math.sqrt((pow(x, 2) + pow(y, 2)))
+
+area = (x + y) / 2
+perimeter = x + y + z
+
+print("Area of right angled triangle = %.2f" % area)
+print("Perimeter of right angled triangle = %.2f" % perimeter)
